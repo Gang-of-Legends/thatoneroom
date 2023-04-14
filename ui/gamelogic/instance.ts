22990fa7cfa7ef211cd7645377106bff7c112ca6
Peter Malina <peter.malina@kiwi.com>
2023-04-14 23:33:37 +0200

    feat(ui): add websocket conn

--- ui/gamelogic/instance.ts
+++ ui/gamelogic/instance.ts
@@ -1,22 +1,63 @@
 import { useState } from "react";
 import Phaser from "phaser";
-import { createConnectTransport, createGrpcWebTransport } from "@bufbuild/connect-web";
-import {
-  WritableIterable,
-  createWritableIterable,
-} from "@bufbuild/connect/protocol";
-import { PartialMessage } from "@bufbuild/protobuf";
 
 export class GameLogic {
   game: Phaser.Game;
 
+  socket: WebSocket | null = null;
   connected: boolean = false;
+  authenticated: boolean = false;
 
   constructor(game: Phaser.Game) {
     this.game = game;
   }
 
   async connect() {
+    const socket = new WebSocket("wss://petermalina.com");
+
+    socket.onopen = () => {
+      this.socket = socket;
+      this.connected = true;
+
+      this.send({
+        type: "authenticate",
+      });
+    }
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      this.handleMessage(data);
+    }
+  }
+
+  handleMessage(data: any) {
+    switch (data.type) {
+      case "authentication":
+        this.handleAuthentication(data);
+        break;
+
+      case "addPlayer":
+        this.addPlayer(data.playerId);
+        break;
+
+      case "movePlayer":
+        this.movePlayer(data.playerId, data.x, data.y);
+        break;
+        
+      case "moveSelf":
+        this.moveSelf(data.x, data.y);
+        break;
+    }
+  }
+
+  send(data: any) {
+    if (this.socket) {
+      this.socket.send(JSON.stringify(data));
+    }
+  }
+
+  handleAuthentication(data: any) {
+    this.authenticated = true;
   }
 
   addPlayer(playerId: string) {}
