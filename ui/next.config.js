6b6f2b56d3013e3ca196c41c9eb0d9ac56fc8863
Peter Malina <peter.malina@kiwi.com>
2023-04-16 11:54:17 +0200

    feat(firebase): add hosting

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
 module.exports = nextConfig
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,6 +1,28 @@
 import dynamic from 'next/dynamic'
 import { CSSProperties } from 'react';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDsA30UYR22NCmjQdX7a97DixD5_QlMwpc",
+  authDomain: "thatoneroom.firebaseapp.com",
+  projectId: "thatoneroom",
+  storageBucket: "thatoneroom.appspot.com",
+  messagingSenderId: "360693127113",
+  appId: "1:360693127113:web:7295e452bfc8308340ba18",
+  measurementId: "G-QJNPDZTKRS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 const Game = dynamic(
   () => import('../components/game'),
   { ssr: false }
